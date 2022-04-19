e680ab4d6b4b17d51499123d3015bc49eeb67ce7
Jesus Rios <jesus.rios@corusconsulting.com>
2022-04-19 14:35:17 -0500

    update
    main2

---
+++ main2.py
@@ -0,0 +1,136 @@
+from turtle import position
+from urllib import response
+import requests
+import re
+from datetime import datetime
+
+#Key API access
+COINMARKET_API_KEY = "89f98e0b-a119-494b-8334-a4a8602d7c24"
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': COINMARKET_API_KEY
+}
+
+#get data from CoinMarketCap
+data = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",headers=headers).json()
+
+#menu INICIO
+optionLogin=["1","2"]
+
+#menu HISTORIAL
+optionHistorical=["1","2"]
+
+#menu PRINCIPAL
+optionMain=["1","2","3","4","5","6"]
+
+regularExp = re.compile(r'^[0-9]+([,][0-9]+)?$')
+
+userList = ["laoming","baki01"]
+passwordList = ["5702471","123456"]
+moneyList = [125,250,352]
+coinList = []
+coin = ""
+newUser = ""
+newPassword = ""
+userLogin = ""
+passwordLogin = ""
+sendMoney = ""
+receivedMoney = ""
+option = ""
+historical = []
+
+#Recorre la data que trae del api de CoinMarketCap y almacena "Symbol" = (BTC,ETH,DOGE entre otros) en coinList
+for symbol in data["data"]:
+    coinList.append(symbol["symbol"])
+    
+#return true where user is valid    
+def isUser(user):
+    return user in userList
+
+#return true where coin is valid    
+def isCoin(coin):
+    return coin in coinList
+
+def optLogin(option):
+    return option in optionLogin
+
+def optMain(option):
+    return option in optionMain
+    
+def optHistorical(option):
+    return option in optionHistorical
+
+def getPosition(value,list):
+    index = list.index(value)
+    return index
+
+def separator():
+    print("------------------------")
+    
+def options():
+    print("1. Send money $")
+    print("2. Received money $")
+    print("3. View coin information")
+    print("4. Deposit money")
+
+    
+    
+
+
+separator()
+print("1. Login")
+print("2. Register")
+separator()
+option = input("Select: ")
+separator()
+    
+while not optLogin(option):
+    print("Invalid option")
+    print("1. Login")
+    print("2. Register")
+    separator()
+    option = input("Select: ")
+else:
+    if (option == optionLogin[0]):
+        userLogin = input("User: ")
+        
+        while not isUser(userLogin):
+            separator()
+            print("Invalid user")
+            separator()
+            userLogin = input("User: ")
+        else:
+            separator()
+            passwordLogin = input("Password: ")
+            index = getPosition(userLogin,userList)
+            while not passwordLogin == passwordList[index]:
+                separator()
+                print("Invalid password")
+                passwordLogin = input("Password: ")
+            else:
+                separator()
+                print("Wellcome",userLogin)
+                print("Your money: ",moneyList[index],"$")
+                separator()
+                options()
+                separator()
+                option = input("Select: ")
+                while not optMain(option):
+                    print("Invalid option")
+                    separator()
+                    options()
+                    separator()
+                    option = input("Select: ")
+                else:
+                    print("OK")
+                    
+                
+                
+            
+#    elif (option == optionLogin[1]):      
+            
+            
+        
+
+    
+    
